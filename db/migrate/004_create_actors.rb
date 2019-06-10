6184cdf226b959fc34da26a1c64a7d858e160674
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-10 16:25:45 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,7 +1,7 @@
 class CreateActors < ActiveRecord::Migration[5.2]
 
   def change
-    create_table :actors do |t|
+    create_table :artists do |t|
       t.string :first_name
       t.string :last_name
     end
