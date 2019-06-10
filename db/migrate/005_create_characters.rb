8bd0c55ea39ada63a31164903582a61b70e22f52
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-10 16:27:49 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -2,7 +2,7 @@ class CreateCharacters < ActiveRecord::Migration[5.2]
   def change
     create_table :characters do |t|
       t.string :name
-      t.stirng :catchphrase
+      t.string :catchphrase
     end
   end
 end
