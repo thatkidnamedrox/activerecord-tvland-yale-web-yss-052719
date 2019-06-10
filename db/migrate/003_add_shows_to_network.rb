840e9d9e9890d9b96601224414b4440c77234db6
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-10 16:33:38 +0000

    Automatically backed up by Learn

--- db/migrate/003_add_shows_to_network.rb
+++ db/migrate/003_add_shows_to_network.rb
@@ -5,5 +5,7 @@ class AddShowsToNetwork < ActiveRecord::Migration[5.2]
   def change
     add_column :shows, :network_id, :integer
     add_column :shows, :genre, :string
+    add_column :shows, :day, :string
+    add_column :shows, :season, :string
   end
 end
